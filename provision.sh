41cfaae794c6401d3099579d9e8d2ca6129bc475
samuel-walters <SWalters@spartaglobal.com>
2022-04-21 12:51:08 +0100

    add app and environment folders

--- provision.sh
+++ provision.sh
@@ -4,4 +4,11 @@ sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get install nginx -y
 sudo systemctl start nginx
-sudo systemctl enable nginx 
+sudo systemctl enable nginx
+cd app
+sudo apt-get install -y nodejs
+sudo apt-get install python-software-properties
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+cd app
+npm install
+npm start
